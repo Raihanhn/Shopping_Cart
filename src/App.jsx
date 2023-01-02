48204e6bd9e02d7c7d0242610cb11fe1a3d1dfc6
raihanhn <raihanhn20@gmail.com>
2023-01-02 20:59:09 +0600

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Navbar from "./components/Navbar"
+ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
 
 function App() {
 
   return (
-    <div className="App">
-     hello bb
+    <div className='App'>
+     <Router>
+     <Navbar/>
+     <Routes>
+      <Route path="/" />
+      <Route path="/cart" />
+     </Routes>
+     </Router>
     </div>
   )
 }
