852cc84dc26368c8f13a57d43dd29baaf6630ff8
raihanhn <raihanhn20@gmail.com>
2023-01-05 15:47:08 +0600

    6th commit

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -28,11 +28,16 @@ const Cart = () => {
         })}
       </div>
 
+      {totalAmount > 0 ? (
+
       <div className="checkout">
         <p> Subtotal: ${totalAmount} </p>
         <button onClick={() => navigate("/")}> Continue Shopping</button>
         <button> Checkout </button>
       </div>
+      ) : (
+        <h1> Your Cart is Empty</h1>
+        )}
 
     </div>
   )
